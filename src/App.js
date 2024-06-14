860ec9b5cf9d7d453b015e0f15ec4e8cb2c43c9e
VSP1000 <surya@criodo.com>
2024-06-14 11:17:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    const handleIncreament = () => (
+        setCount(count + 1)
+    )
+
+    const handleDecreament = () => (
+        setCount(count - 1)
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex justify-content-start align-items-center' style={{height: "100vh"}}>
+        <div>
+          <h1>Counter App</h1>
+        <p className='text-center style={{fontSize: "70px"'>count: {count}</p>
+            <div className='d-flex'>
+                <button onClick={handleIncreament} className='btn btn-success me-3'>Increment</button>
+                
+                <button onClick={handleDecreament} className='btn btn-danger ms-3'>Decrement</button>
+            </div>
+        </div>
+                
     </div>
-  );
+                
+  )
 }
-
-export default App;
